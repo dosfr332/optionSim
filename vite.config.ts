115ca34460a9b4160c8344d8c010954aefc078bb
Freddie Dossor <freddossor@gmail.com>
2025-01-31 23:29:59 +1300

    fixing deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
-  base: "/vite-react-deploy/",
+  base: "/option-sim/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
