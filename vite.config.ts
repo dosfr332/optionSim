165b01a0f9fbdfa59a482dd742c41944517814b2
Freddie Dossor <freddossor@gmail.com>
2025-02-01 11:52:32 +1300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
-  base: "/",
+  base: "/optionSim/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
