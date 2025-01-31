a52710f121f84b03ad7f239cfcf8625f87808471
Freddie Dossor <freddossor@gmail.com>
2025-02-01 11:43:34 +1300

    update title and favicon

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
-  base: "/option-sim/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
